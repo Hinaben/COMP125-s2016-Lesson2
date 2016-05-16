8f50130d91a05b05029bdb69b65ee3706f8587ec
Hinaben <henapatel21@hotmail.com>
2016-05-16 09:41:56 -0400

    initial commit

---
+++ Scripts/app.js
@@ -0,0 +1,14 @@
+/* Main JavaScript file */
+
+// IIFF - Immediately invokked function expression
+(function(){
+    "use strict";
+    //code codes in here
+    console.log("App Started..")
+    
+    
+    
+    
+    
+    
+})();
